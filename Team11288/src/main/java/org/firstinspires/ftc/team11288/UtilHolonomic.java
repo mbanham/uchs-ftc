255d7b157f7d6485b3584f9efc5d7b5cd2da99c8
mbanham <mbanham@gmail.com>
2020-02-05 14:56:36 -0800

    changes from 2/5/2020-center distance

--- Team11288/src/main/java/org/firstinspires/ftc/team11288/UtilHolonomic.java
+++ Team11288/src/main/java/org/firstinspires/ftc/team11288/UtilHolonomic.java
@@ -49,7 +49,7 @@ public class UtilHolonomic {
     public static double ROBOT_WALL_CLEARANCE = 5;//clearance to put the robot from the edge
     public static double WALL_ROBOT_TO_EDGE_LOAD = 47;//distance from the robot to the edge with slipping and a load
     public static double EDGE_TO_PLATFORM_CLEARANCE = EDGE_TO_PLATFORM+4 - ROBOT_WALL_CLEARANCE;//distance from the robot to the clearance distance
-    public static double WALL_TO_CENTER = 30;//distance from the robot to the clearance distance
+    public static double WALL_TO_CENTER = 29;//distance from the robot to the clearance distance
 
     public static double AUTONOMOUS_SPEED = 1;//speed for the autonomous programs
 
