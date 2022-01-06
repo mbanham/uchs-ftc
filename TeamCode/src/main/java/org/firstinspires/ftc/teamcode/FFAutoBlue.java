cf11c9726478d35fde2280b81496df8b83a33082
forkpoweroutlet <48189055+forkpoweroutlet@users.noreply.github.com>
2022-01-05 21:44:44 -0800

    Begin work on inner blue auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FFAutoBlue.java
@@ -1,75 +1,126 @@
 package org.firstinspires.ftc.teamcode;
 
-
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Utilities.UtilHolonomic;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
-
-
-@Autonomous(name = "FFAutoBlue", group = "Linear Opmode")
+// Asynchronous state machine for auto (see https://gm0.org/en/latest/docs/software/finite-state-machines.html)
+@Autonomous(group = "advanced")
 public class FFAutoBlue extends LinearOpMode {
-    //initialize these variables, override them in the constructor
 
-    /* Declare OpMode members. */
-    //wheels
-    private DcMotor motorFrontRight;
-    private DcMotor motorFrontLeft;
-    private DcMotor motorBackRight;
-    private DcMotor motorBackLeft;
+    // This enum defines our "state"
+    // This is essentially just defines the possible steps our program will take
+    enum State {
+        SPACE_FORWARD,
+        GO_TO_CAROUSEL,
+        SPIN_CAROUSEL,
+        GO_TO_STORAGE,
+        IDLE            // Our bot will enter the IDLE state when done
+    }
 
-    private DcMotor carouselSpinner;
+    // We define the current state we're on
+    // Default to IDLE
+    State currentState = State.IDLE;
 
-    private UtilHolonomic teamUtils;
-
+    // Define our start pose
+    // This assumes we start at x: 15, y: 10, heading: 180 degrees
+    Pose2d startPose = new Pose2d(-36, 61, Math.toRadians(270));
 
     @Override
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+    public void runOpMode() throws InterruptedException {
+        // Initialize carousel spinner
+        DcMotor carouselSpinner = hardwareMap.dcMotor.get("carousel spinner");
 
-        //initialize wheels
-        motorFrontRight = hardwareMap.dcMotor.get("motor front right");
-        motorFrontLeft = hardwareMap.dcMotor.get("motor front left");
-        motorBackLeft = hardwareMap.dcMotor.get("motor back left");
-        motorBackRight = hardwareMap.dcMotor.get("motor back right");
-        carouselSpinner = hardwareMap.dcMotor.get("carousel spinner");
-        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBackRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        // Initialize SampleMecanumDrive & set initial pose
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(startPose);
 
-        //utils class initializer
-        teamUtils = new UtilHolonomic(motorFrontRight, motorFrontLeft, motorBackRight, motorBackLeft, telemetry);
+        Trajectory trajectory1 = drive.trajectoryBuilder(startPose)
+                .forward(4)
+                .build();
 
-        // Wait for the game to start (driver presses PLAY)
+        Trajectory trajectory2 = drive.trajectoryBuilder(trajectory1.end())
+                .lineToLinearHeading(new Pose2d(-56, 60, 0))
+                .build();
+
+        Trajectory trajectory3 = drive.trajectoryBuilder(trajectory2.end())
+                .lineTo(new Vector2d(-62, 36))
+                .build();
+
+        // For waiting
+        ElapsedTime waitTimer = new ElapsedTime();
+
+
         waitForStart();
-        //Play started
+        if (isStopRequested()) return;
 
-        resetStartTime();
-        while (opModeIsActive()) {
-                // run this loop until the end of the match (driver presses stop)
-            int currentTime = (int) getRuntime();
+        // trajectory1: Move forward to allow turning
+        currentState = State.SPACE_FORWARD;
+        drive.followTrajectoryAsync(trajectory1);
 
-            // spin motor every other second
-            carouselSpinner.setPower(currentTime    % 2 == 0 ? 0.4 : 0.0);
+        while (opModeIsActive() && !isStopRequested()) {
 
-            // start after 12s
-            if(currentTime <= 12) {
-                continue;
+            // State machine
+            switch (currentState) {
+
+                case SPACE_FORWARD:
+
+                    // drive.isBusy == false when trajectory completed
+                    if (!drive.isBusy()) {
+                        currentState = State.GO_TO_CAROUSEL;
+                        drive.followTrajectoryAsync(trajectory2);
+                    }
+                    break;
+                case GO_TO_CAROUSEL:
+
+                    if(!drive.isBusy()) {
+                        currentState = State.SPIN_CAROUSEL;
+                        waitTimer.reset();
+                    }
+                    break;
+                case SPIN_CAROUSEL:
+
+                    // spin carousel for 4 seconds
+                    carouselSpinner.setPower(0.4);
+                    drive.setMotorPowers(-0.1, -0.1, -0.1, -0.1);
+
+
+                    if (waitTimer.seconds() >= 4) {
+                        carouselSpinner.setPower(0.0);
+                        drive.setMotorPowers(0.0, 0.0, 0.0, 0.0);
+                        currentState = State.GO_TO_STORAGE;
+                        drive.followTrajectoryAsync(trajectory3);
+                    }
+                    break;
+                case GO_TO_STORAGE:
+
+                    if(!drive.isBusy()) {
+                        currentState = State.IDLE;
+                    }
+                    break;
+                case IDLE:
+                    // Done, do nothing
+                    break;
             }
-            teamUtils.DriveByDistance(1, 1, 12);
-            requestOpModeStop();
 
-            }
+            // We update drive continuously in the background, regardless of state
+            drive.update();
+
+            // Read pose
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            // Print pose to telemetry
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+            telemetry.update();
         }
+    }
+
 }
-
-
